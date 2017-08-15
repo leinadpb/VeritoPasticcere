81457763e947581e1e5f38b8e12bae245fd3ed7e
Daniel Peña <daniel.pena@intec.edu.do>
2017-08-15 18:56:17 -0400

    404 page improved

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,1 +1,19 @@
-<div class="container" align="center"><img class="img-responsive img-fluid" src="{{URL::to('images/404.jpg')}}" style="width:100em;height:auto;"></div>
+@extends('layouts.master-normal-others')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div align="center">
+            <div class="display-1">404</div>
+            <div class="display-3">Página no encontrada</div><hr>
+            <div><h5>
+                Lo sentimos pero la página que estás buscando no existe en nuestro sitio web.<br><br>
+                    <b>ATT: Verito Pasticcere.</b> <i>Todos los derechos reservados.</i></h5>
+            </div><br>
+            <div>
+                <a href="{{route('home')}}">www.verito.com</a>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
